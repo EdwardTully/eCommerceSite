608e79cd436c6a620493ea39de4790810dea0b2d
edward tully <edwardTully1998@gmail.com>
2026-01-26 17:51:13 -0500

    kgbkg

--- netlify/functions/get-products.js
+++ netlify/functions/get-products.js
@@ -4,13 +4,8 @@ const sql = neon();
 
 
 export default async (req, context) => {
-  // Fetch all products and join the first image (display_order = 1) if available
-  const products = await sql`
-    SELECT p.*, i.blob_key
-    FROM products p
-    LEFT JOIN images i ON i.product_id = p.id AND i.display_order = 1
-    ORDER BY p.id DESC
-  `;
+  // Fetch all products from the products table
+  const products = await sql`SELECT * FROM products ORDER BY id DESC`;
   return new Response(JSON.stringify(products), {
     headers: { "Content-Type": "application/json" }
   });
